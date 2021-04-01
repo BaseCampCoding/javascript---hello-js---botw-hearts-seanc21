96210342d4560d80792c86945880fd37152e7273
Sean Coh <scoh21@basecampcodingacademy.org>
2021-04-01 10:41:07 -0500

    heal button

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const heartsContainer = document.querySelector("#hearts-container");
 const controlsContainer = document.querySelector("#controls-container");
 const hitButton = controlsContainer.querySelector("#hit-button");
 const hitDamageInput = controlsContainer.querySelector("#hit-damage-input");
+
+const healButton =  controlsContainer.querySelector("#heal-button");
+const healAmountInput = controlsContainer.querySelector("#heal-amount-input");
+
+const addHeartButton = controlsContainer.querySelector("#add-heart-container-button");
+
 let health = 35;
 let maxHealth = 40;
 
@@ -27,3 +33,15 @@ hitButton.addEventListener("click", function () {
   health = Math.max(0, health - damage);
   updateHeartsDisplay();
 });
+
+healButton.addEventListener("click", function() {
+  let heal = Number(healAmountInput.value);
+  health = Math.min(health + heal, maxHealth + 4);
+  updateHeartsDisplay();
+});
+
+// addHeartButton.addEventListener("click", () => {
+//   maxHealth += 4;
+//   health = Math.max(health + 4, maxHealth);
+//   updateHeartsDisplay();
+// });
